355ab148da9cc541798a656ec3e8913861ad19f6
KsyTim <timoshenko.ksenia.1998@gmail.com>
2022-11-09 21:22:23 +0400

    added header section

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,10 @@ const gulp = require("gulp");
 const sass = require('sass');
 const gulpSass = require('gulp-sass');
 const scssCompiler = gulpSass(sass);
-const prefixer = require('gulp-autoprefixer');
+// const autoprefixer = require('gulp-autoprefixer');
+// const postcss = require('gulp-postcss');
 gulp.task('styles', () => {
   return gulp.src('./src/scss/**/*.scss')
-             .pipe(prefixer({
-                overrideBrowserslist: ['last 8 versions'],
-                browsers: [
-                  'Android >= 4',
-                  'Chrome >= 20',
-                  'Firefox >= 24',
-                  'Explorer >= 11',
-                  'iOS >= 6',
-                  'Opera >= 12',
-                  'Safari >= 6',
-                ],
-                cascade: false
-              }))
              .pipe(scssCompiler().on('error', scssCompiler.logError))
              .pipe(gulp.dest('./dist/css'))
 })
